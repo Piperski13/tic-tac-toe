3b9e188373e68ccebc2756c7100e8576935af72b
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-04-13 17:48:27 +0200

    Version 2.2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//restart
+const gridElement = document.querySelectorAll('.grid-element');
+const restartBtn = document.querySelector('.restartBtn');
+const statusText = document.querySelector('.statusText');
+const winningCondition = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+let options = ["","","","","","","","",""];
+let currentPlayer = "X";
+let running = false;

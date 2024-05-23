427cfa5341c78ee35ea3e01be5c93e57aa4501d2
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-23 10:06:33 +0200

    Version 3.3(cleand up)

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
   res.status(200).sendFile(__dirname,'public','index.html');
-})
+});
 
 const port = 3000;
 app.listen(port,()=>{
   console.log(`Port ${port} is listening for a request...`);
-})
+});

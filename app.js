7e54f77ea8c2d6c3b03a1d140b8eab6209f71b44
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-23 10:05:43 +0200

    Version 3.2(added server and maped out public files on server)

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+  res.status(200).sendFile(__dirname,'public','index.html');
+})
+
+const port = 3000;
+app.listen(port,()=>{
+  console.log(`Port ${port} is listening for a request...`);
+})
